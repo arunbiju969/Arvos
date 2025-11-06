ee43ceba496a62ce1124f12002128eff6634dbc9
Jaskirat Singh <jaskirat880singh@gmail.com>
2025-11-06 15:38:31 -0800

    first commit

---
+++ arvos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  arvos
+//
+//  Created by JASKIRAT SINGH on 2025-11-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ arvos/arvosApp.swift
@@ -0,0 +1,17 @@
+//
+//  arvosApp.swift
+//  arvos
+//
+//  Created by JASKIRAT SINGH on 2025-11-06.
+//
+
+import SwiftUI
+
+@main
+struct arvosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
